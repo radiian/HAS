100d1321e94800854df80926dfdefa21e33e9d78
Zach <thompzac@oregonstate.edu>
2017-02-26 03:01:20 -0800

    Majority of socket functionality written. Nothing tested yet.

--- sock.cpp
+++ sock.cpp
@@ -1,4 +1,4 @@
-#include <sock.h>
+#include "sock.h"
 #include <iostream>
 #include <string>
 #include <sys/types.h>
@@ -6,3 +6,50 @@
 #include <netinet/in.h>
 #include <netinet/ip.h>
 #include <unistd.h>
+
+
+Sock::Sock(sock_type _type, std::string address, int port){
+	sock_addr.sin_family = AF_INET;
+	sock_addr.sin_port = htons(port);
+	inet_pton(AF_INET, address.c_str(), &sock_addr.sin_addr.s_addr);
+	mode = 1;
+	type = _type;
+	if(type == udp){
+		sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	}
+	else{
+		if(type == tcp){
+			sockfd = socket(AF_INET, SOCK_STREAM, 0);
+		}
+	}
+}
+
+
+
+
+Sock::Sock(sock_type _type, int port){
+	sock_addr.sin_family = AF_INET;
+	sock_addr.sin_port = htons(port);
+	sock_addr.sin_addr.s_addr = INADDR_ANY;
+	mode = 0;
+	type = _type;
+	if(type == udp){
+		sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	}
+	else{
+		if(type == tcp){
+			sockfd = socket(AF_INET, SOCK_STREAM, 0);
+		}
+	}
+}
+
+void Sock::Bind(){
+	if(bind(sockfd, (struct sockaddr *)&sock_addr, sizeof(sock_addr)) < 0)
+	{
+		lasterror = errno;
+	}
+}
+
+void Sock::Listen(int backlog){
+	listen(sockfd, backlog);
+}
--- sock.h
+++ sock.h
@@ -1,5 +1,13 @@
-#IF NDEF SOCK_HEADER
-#DEFINE SOCK_HEADER
+#ifndef SOCK_HEADER
+#define SOCK_HEADER
+#include <string>
+#include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <unistd.h>
+#include <arpa/inet.h>
 
 enum sock_type{
 	tcp,
@@ -10,12 +18,25 @@ class Sock{
 	int sockfd = 0;//File descriptor for the socket
 	int lasterror = 0;//The last error the socket returned
 	int port = 0;//The port to connect to or listen on
-	string ip_address = "";//The IP address to connect to or liston on
+	int mode = 0;//Socket mode. 0 = server, 1 = client;
+	int connect = 0;//0 = connected, 1 = disconnected
+	int maxConnections = 10;//Max connections for server to handle
+	std::string ip_address = "";//The IP address to connect to or liston on
 	sock_type type;//Type of socket to create. tcp or udp
 	struct sockaddr_in sock_addr;//The address struct for the socket
 	
-}
+
+	Sock(sock_type _type, std::string address, int port);//For client
+	Sock(sock_type, int);//For server
+	void Close();
+	void Bind();
+	void Listen(int);
+	void Connect();
+	Sock Accept();
+	void write(std::string);
+	std::string read(Sock);
+};
 
 
 
-#ENDIF
+#endif
