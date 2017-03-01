965e359021797eb6aab732ecc7c2fdb58ab77e89
Zach <thompzac@oregonstate.edu>
2017-02-28 17:53:24 -0800

    Found good demo for sockets with poll()

--- demo/test.cpp
+++ demo/test.cpp
@@ -2,6 +2,11 @@
 #include <thread>
 #include "../src/sock.h"
 #include <fcntl.h>
+#include <poll.h>
+
+
+//Great example of nonblocking sockets using poll
+//http://www.ibm.com/support/knowledgecenter/ssw_ibm_i_71/rzab6/poll.htm
 
 void call(Sock *s, bool *running){
    	std::cout << "From thread!" << std::endl;
@@ -10,9 +15,9 @@ void call(Sock *s, bool *running){
 	std::cout << "We are listening!" << std::endl;
 	while(*running == true){
 		try{   
-		   (*s).Accept();
+		   Sock reader = (*s).Accept();
 		   std::cout << "Client connected!" << std::endl;
-	   }
+		}	
 	   catch(SNoAccept &e){}//Do nothing as the socket is non blocking
 	   catch(SError &e){
 		  std::cout << e.what() << std::endl;
@@ -22,12 +27,22 @@ void call(Sock *s, bool *running){
 }
 
 int main(){
-   	bool isrun = true;
+   	
+	//struct pollfd pfd;
+	
+   
+    bool isrun = true;
 	bool *r = &isrun;
    	Sock tmp = Sock(tcp, 28016);
 	fcntl(tmp.sockfd, F_SETFL, O_NONBLOCK);
 	Sock* tmpptr = &tmp;
-   	std::thread test(call, tmpptr, r);
+   	//setup for poll()
+	//pfd.fd = tmp.sockfd;
+	//pfd.events = POLLIN;
+	//poll(&pfd, 1, 100);
+	//
+	
+	std::thread test(call, tmpptr, r);
 	std::string msg = "";
 	while(true){
 		std::cout << "Break? [y\n] ";
